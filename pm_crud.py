9fe4a34ae67cdd84470c6980eb6919b3ab4ba117
Czarap <overriderlightborn11104@gmail.com>
2024-11-16 17:19:38 +0800

    2nd cm

--- pm_crud.py
+++ pm_crud.py
@@ -3,7 +3,7 @@ from http import HTTPStatus
 
 app = Flask(__name__)
 
-
+#sample data
 books = [
     {"id": 1,
      "title": "The Great Gatsby",
@@ -21,13 +21,15 @@ def find_book(book_id):
             return book
     return None
 
+
+#Read
 @app.route("/api/books", methods=["GET"])
 def get_books():
     return jsonify({"success": True, 
                     "data": books, 
                     "total": len(books)}), HTTPStatus.OK
 
-@app.route("/api/books/book_id", methods=["GET"])
+@app.route("/api/books/<int:book_id>", methods=["GET"])
 def get_book(book_id):
 
     book = find_book(book_id)
@@ -43,6 +45,8 @@ def get_book(book_id):
         )
 
     return jsonify({"success": True, "data": book}), HTTPStatus.OK
+
+#Create
 
 @app.route("/api/books", methods=["POST"])
 def create_book():
@@ -67,7 +71,7 @@ def create_book():
             }), HTTPStatus.BAD_REQUEST
 
     new_book = {
-        "id": max(book['id'] for book in books) + 1, 
+        "id": max(book["id"] for book in books) + 1, 
         "title": data["title"],
         "author": data["author"],
         "year": data["year"]
@@ -79,6 +83,36 @@ def create_book():
         "success": True,
         "data": new_book
     }), HTTPStatus.CREATED
+
+
+# Update
+@app.route("/api/books/book", methods=["PUT"])
+def update_book(book_id):
+    book = find_book(book_id)
+    if book is None:
+        return (
+            jsonify({"success": False, 
+                     "error": "Book not found"}),
+            HTTPStatus.NOT_FOUND,
+        )
+
+    if not request.is_json:
+        return (
+            jsonify(
+                {"success": False, 
+                 "error": "Content-type must be application/json"}
+            ),
+            HTTPStatus.BAD_REQUEST,
+        )
+
+    data = request.get_json()
+
+    book["title"] = data.get("title", book["title"])
+    book["author"] = data.get("author", book["author"])
+    book["year"] = data.get("year", book["year"])
+
+    return jsonify({"success": True, "data": book}), HTTPStatus.OK
+
 
 if __name__ == "__main__":
     app.run(debug=True)
