89afe784e0c5cbec7934bf7fd2aa1dd973902f9c
Czarap <120724387+Czarap@users.noreply.github.com>
2024-11-18 16:09:17 +0800

    Delete gg.sql

--- gg.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE book_db;
-Use book_db ;
-CREATE TABLE books (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    author VARCHAR(255) NOT NULL,
-    year INT NOT NULL
-);
